d38c45058f8675b187fb3d8d2ea309ba775ad47e
test <test>
2025-02-15 12:45:06 +0600

    Some Changes

--- main.py
+++ main.py
@@ -3,14 +3,12 @@ import random
 
 n = random.randint(1, 100)
 a = -1
-guesses = 0
+guesses = 1
 while(a != n):
-    guesses += 1
     a = int(input("Guess a number: "))
     if (a > n):
         print("Lower Number Please")
     elif(a < n):
         print("Higer Number Please")
-    elif(a == n):
-        break
+    guesses += 1
 print(f"You Find the number in {guesses} guesses")
